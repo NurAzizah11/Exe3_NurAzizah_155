401d7b955706632db4d781a48eea984c5462e80b
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-11-23 15:44:55 +0700

    menambahkan public bool Search

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -22,8 +22,18 @@ namespace Exercise3
             LAST = null;
         }
 
-        
-    }
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*returns true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*If the node is presents at the end*/
+                return true;
+            else
+                return (false);/*returns false if the node is not found*/
+        }
     }
 }
 
